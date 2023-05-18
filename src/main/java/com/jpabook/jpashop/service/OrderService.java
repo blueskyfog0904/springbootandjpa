53b6679c3fa3903aae4f84f288e437a811fdba62
blueskyfog0904 <blueskyfog@gmail.com>
2023-05-18 18:31:06 +0900

    Inflearn lecture for Spring boot and JPA. > develop web layer >> Order >> OrderList and order cancel

--- src/main/java/com/jpabook/jpashop/service/OrderService.java
+++ src/main/java/com/jpabook/jpashop/service/OrderService.java
@@ -4,6 +4,7 @@ import com.jpabook.jpashop.domain.*;
 import com.jpabook.jpashop.repository.ItemRepository;
 import com.jpabook.jpashop.repository.MemberRepository;
 import com.jpabook.jpashop.repository.OrderRepository;
+import com.jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -56,10 +57,10 @@ public class OrderService {
     }
 
     // 검색
-    /*
+
     public List<Order> findOrders(OrderSearch orderSearch) {
-        return orderRepository.findAll(orderSearch);
+        return orderRepository.findAllByString(orderSearch);
     }
 
-    */
+
 }
